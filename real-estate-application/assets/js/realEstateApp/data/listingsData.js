c7672a7259d1b63966a3f774bea2b83844875ff1
Jitendra Nishad <53356706+Jitendra-coder-max@users.noreply.github.com>
2023-05-25 09:35:47 -0400

    pushed the real estate code

---
+++ real-estate-application/assets/js/realEstateApp/data/listingsData.js
@@ -0,0 +1,102 @@
+var listingsData = [
+  {
+    address:'Lave view point',
+    city: 'Bhopal',
+    state: 'MP',
+    rooms: 1,
+    price: 220000,
+    floorSpace:2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    houseType:'Apartment',
+    image:'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4147-23/the-hesby-apartments-exterior.jpg'
+  },
+  {
+      address:'Patel Nagar',
+      city: 'Indore',
+      state: 'MP',
+      rooms: 0,
+      price: 50000,
+      floorSpace:700,
+      extras: [
+        'elevator',
+        'pool'
+      ],
+      houseType:'Studio',
+      image:'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4022-48/sakura-crossing-apartments-exterior.jpg'
+    },
+    {
+      address:'Nehru Nagar',
+      city: 'Gwalior',
+      state: 'MP',
+      rooms: 3,
+      price: 220000,
+      floorSpace:2500,
+      extras: [
+        'pool',
+        'gym'
+      ],
+      houseType:'Condo',
+      image:'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/3639-12/mosaic-at-metro-apartments-swimming-pool.jpg'
+    },
+    {
+      address:'Vaskodigama',
+      city: 'Pangi',
+      state: 'Goa',
+      rooms: 3,
+      price: 300000,
+      floorSpace:1400,
+      extras: [
+        'elevator',
+        'gym'
+      ],
+      houseType:'Condo',
+      image:'https://az616578.vo.msecnd.net/files/2016/07/26/636050962570138758-2091420308_Elegant-apartment-finder.jpg'
+    },
+    {
+      address:'Sair sapata',
+      city: 'Bhopal',
+      state: 'MP',
+      rooms: 3,
+      price: 160000,
+      floorSpace:2000,
+      extras: [
+        'elevator',
+        'gym'
+      ],
+      houseType:'Ranch',
+      image:'https://cdn.vox-cdn.com/thumbor/vAZEMsqZIJsmKcT8paqeFY1xI80=/0x0:1200x800/1200x800/filters:focal(504x304:696x496)/cdn.vox-cdn.com/uploads/chorus_image/image/56604061/090817wBroadway.0.jpg'
+    },
+    {
+      address:'Vaskodigama',
+      city: 'Pangi',
+      state: 'Goa',
+      rooms: 2,
+      price: 100000,
+      floorSpace:2000,
+      extras: [
+        'elevator',
+        'pool'
+      ],
+      houseType:'Condo',
+      image:'https://cdn.vox-cdn.com/thumbor/NeX2IvnaTUvJ8UtQ_yLI-jo3gOE=/0x0:2600x1733/1200x800/filters:focal(1092x659:1508x1075)/cdn.vox-cdn.com/uploads/chorus_image/image/60722051/1300PikeSt_Cam3People_111816.0.png'
+    },
+    {
+      address:'Avenue city',
+      city: 'Mumbai',
+      state: 'Maharastra',
+      rooms: 1,
+      price: 100000,
+      floorSpace:2000,
+      extras: [
+        'elevator',
+        'pool'
+      ],
+      houseType:'Studio',
+      image:'http://photos.mredllc.com/photos/property/434/09300434.jpg'
+    }
+]
+
+export default listingsData;
